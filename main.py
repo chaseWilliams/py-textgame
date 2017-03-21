f17b11c2c1b85681c185ec36c618879cb1aecf12
chaseWilliams <bornofawesomeness@gmail.com>
2017-03-21 14:03:17 -0400

    world envs initialized

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 from lib.controller import Controller
 import json
+import random
 
 game = Controller()
 game.start_game()
+
 # beginning scene
 begin_file = open('txt/begin.txt', 'r')
 begin_dialogue = list(begin_file)
 print(begin_dialogue[0])
 
 envs = open('lib/environment.json')
-print(json.loads(envs.read()))
-"""
-episodes = 2
-for i in range(episodes):
-    random_env = 
-"""
+data = json.loads(envs.read())
+episodes = data['episodes']
+
+def view(episode):
+    print(episode['desc'])
+    actions = list(episode['actions'].keys())
+    possible_choices = range(len(actions))
+    action_map = dict(zip(possible_choices, actions))
+    for i in possible_choices:
+        key = action_map[i]
+        print("{0}: {1}".format(i, key))
+    return action_map
+
+def play_episode(episode, choice, action_map):
+    action = action_map[choice]
+    scenario = episode['actions'][action]
+    result = scenario[1]
+    if result == 'walk':
+        print(scenario[0])
+    elif result == 'monster':
+        print(scenario[0])
+        game.fight()
+        print(scenario[2])
+
+num_episodes = 2
+for i in range(num_episodes):
+    random_episode = random.choice(episodes)
+    action_map = view(random_episode)
+    choice = int(input(">"))
+    play_episode(random_episode, choice, action_map)
+
 # ending scene
 end_file = open('txt/end.txt', 'r')
 end_dialogue = list(end_file)
